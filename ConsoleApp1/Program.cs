1a7fd5d90e42da5c730f35d35496f22bbda41cae
VeekshitaPudi <pudiveekshita580@gmail.com>
2022-02-18 13:41:23 +0530

    Third Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleApp1
 
             for(int i = 0; i < 10; i++)
             {
-                for (int i = 0; i < 60; i++)
+                for (int k = 0; k < 60; k++)
                 {
                     Console.Write("*");
                 }
